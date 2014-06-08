7cae5a14b7e783683fe4324f5c67e6f4747995ed
ardinor <temnoyume@gmail.com>
2014-06-08 17:12:40 +1000

    Some improvements to the script, removed need to email template

--- Scripts/notifyUpdates.sh
+++ Scripts/notifyUpdates.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-var=$(sudo apt-get --simulate dist-upgrade)
-email_add="<address>"
+to_email="<address>"
+from_email="<address>"
+subject="Updates found for "
 
-if [[ "$var" =~ "packages will be upgraded" ]]
+update_result=$(sudo apt-get --simulate dist-upgrade)
+
+if [[ "$update_result" =~ "packages will be upgraded" ]]
 then
         echo "upgrade found"
+        hostname=$(hostname)
         uname=$(uname -a)
         uptime=$(uptime)
         date=$(date)
-        cp email_template.txt email
+        subject="$subject $hostname"
+        touch email
+        echo -e "To:$to_email" >> test
+        echo -e "From:$from_email" >> test
+        echo -e "Subject:$subject \n" >> test
         echo -e "$uname \n" >> email
         echo -e "Uptime: $uptime\n\n" >> email
         echo -e "Updates found at: $date\n\n" >> email
-        echo "$var" >> email
-        sudo ssmtp $email_add < email
+        echo "$update_result" >> email
+        ssmtp $email_add < email
+        rm email
 fi
