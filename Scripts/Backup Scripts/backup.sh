104319ab1c713fee4c6feb31f8c61c9a85f48944
ardinor <temnoyume@gmail.com>
2014-07-22 16:11:42 +1000

    Fixed up incorrect variable name

--- Scripts/Backup Scripts/backup.sh
+++ Scripts/Backup Scripts/backup.sh
@@ -3,7 +3,7 @@
 source backup_vars.sh
 
 while MODDED_DIR=$(inotifywait -r -e modify,attrib,close_write,move,create,delete -q --format %w ${DIRS[@]}); do
-    NRPATH=$(dirname `readlink -m $i`)
+    NRPATH=$(dirname `readlink -m $MODDED_DIR`)
 	# We want a directory structure like /remote/server/var/www/ so get the base path and add it to the base remote path
     NRPATH="$RPATH$NRPATH"
 	# The directory structure should already exist as we created it in initial_backup.sh
