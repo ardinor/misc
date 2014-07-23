779c272b7503098bb9b94f96939302c212c90932
ardinor <temnoyume@gmail.com>
2014-07-23 15:22:55 +1000

    Use the whole date for comparison

--- Scripts/Backup Scripts/archive_backups.sh
+++ Scripts/Backup Scripts/archive_backups.sh
@@ -3,9 +3,11 @@
 BACKUP_DIR=/path/to/backup/dir
 ARCHIVE_DIR=/path/to/archive/dir
 
-BACKUP_MOD_DATE=$(find $BACKUP_DIR -type f -printf '%TY-%Tm-%Td\n' | sort -r | head -n 1)
-ARCHIVE_MOD_DATE=$(find $ARCHIVE_DIR -type f -printf '%TY-%Tm-%Td\n' | sort -r | head -n 1)
+# Find the most recently modified file in both directories and store the date
+BACKUP_MOD_DATE=$(find $BACKUP_DIR -type f -printf '%TY-%Tm-%Td %TT\n' | sort -r | head -n 1)
+ARCHIVE_MOD_DATE=$(find $ARCHIVE_DIR -type f -printf '%TY-%Tm-%Td %TT\n' | sort -r | head -n 1)
 
+# In order to compare the dates, convert them to seconds since 1970
 BACKUP_MOD_SEC=$(date -d "$BACKUP_MOD_DATE" '+%s')
 ARCHIVE_MOD_SEC=$(date -d "$ARCHIVE_MOD_DATE" '+%s')
 
